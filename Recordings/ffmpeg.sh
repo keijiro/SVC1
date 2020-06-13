9c618072195a5dd6c62496d6677df943cd1e5465
Keijiro Takahashi <keijiro@gmail.com>
2020-06-13 18:11:40 +0900

    Add ffmpeg recorder

---
+++ Recordings/ffmpeg.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+encode()
+{
+  ffmpeg -y \
+    -i $1.mov \
+    -preset veryslow \
+    -crf 10 \
+    -pix_fmt yuv420p \
+    -vf 'tmix=frames=3:weights="1 2 1"' \
+    -r 30 \
+    $1.mp4
+}
+
+encode Cut1
